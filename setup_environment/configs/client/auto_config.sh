87bd4d34eb3ed46d9fd9ccbdb090db360dfac345
Toolf <nazar.smirnov.2001@gmail.com>
2022-06-09 21:15:52 +0300

    Add configs

--- setup_environment/configs/client/auto_config.sh
+++ setup_environment/configs/client/auto_config.sh
@@ -1,14 +1,10 @@
 # enp0s3
-ip route add table enp0s8 to 172.16.3.0/24 dev enp0s3 scope link
+
+ip route add table 101 to 172.16.3.0/24 dev enp0s3 metric 200
+ip route add table 101 default via 172.16.3.4 dev enp0s3
+ip rule add from 172.16.3.4 table 101
 
 # enp0s8
-ip route add table enp0s8 to 172.16.4.0/24 dev enp0s8 scope link
-
-ip route add table enp0s8 default via 172.16.1.4 dev enp0s8
-ip rule add from 172.16.1.4 table enp0s8
-
-# enp0s9
-ip route add table enp0s9 to 172.16.2.0/24 dev enp0s9 scope link
-
-ip route add table enp0s9 default via 172.16.2.4 dev enp0s9
-ip rule add from 172.16.2.4 table enp0s9
+ip route add table 102 to 172.16.4.0/24 dev enp0s8 metric 200
+ip route add table 102 default via 172.16.4.4 dev enp0s8
+ip rule add from 172.16.4.4 table 102
